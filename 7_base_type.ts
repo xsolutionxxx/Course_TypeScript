2b049f4b192e91c44768566d9e3a8f2f824b5f3e
xsolutionxxx <nazartalaievych@gmail.com>
2026-01-14 14:44:15 +0200

    base_func

---
+++ 7_base_type.ts
@@ -0,0 +1,20 @@
+
+/* let userName: string = "Alice"; */
+
+// userName.isInteger();
+
+// console.log(`User name is: ${userName}`);
+
+// '' "" ``
+// 10, 0.5, 0.00001, -50, 4e10 Infinity, -Infinity, NaN
+// true, false
+
+const isBirthday: boolean = true;
+let age: number;
+const userName: string = "Bob";
+
+age = 40;
+
+if (isBirthday) {
+  console.log(`Happy Birthday, ${userName.toUpperCase()}! You are now ${age + 1} years old.`);
+}
